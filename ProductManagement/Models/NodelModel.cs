8d79b16c03594c7c28c974b033fb22b285ba7ddc
hecongcong <827422876@qq.com>
2019-05-08 15:09:37 +0800

    model 完善

--- ProductManagement/Models/NodelModel.cs
+++ ProductManagement/Models/NodelModel.cs
@@ -8,14 +8,16 @@ namespace ProductManagement.Models
     public class NodelModel
     {
         public int id { get; set; }
+
         public int? pid { get; set; }
+
         public string title { get; set; }
 
         public string font { get; set; }
 
         public string url { get; set; }
 
-        public string param { get; set; } = "";
+        public string param { get; set; } = string.Empty;
 
         public string condition { get; set; }
 
