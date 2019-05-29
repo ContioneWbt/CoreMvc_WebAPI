0871fef1fa8327714b92dd0e55d3c0cb3247d6c4
hecongcong <827422876@qq.com>
2019-05-29 11:21:15 +0800

    VGA是

---
+++ H_Blog/Attributes/ExceptionHandleAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net.Http;
+namespace H_Blog.Attributes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ActionFilterInvokeAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext actionExecutedContext)
+        {
+            base.OnActionExecuting(actionExecutedContext);
+        }
+    }
+    /// <summary>
+    /// 异常处理
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Method)]
+    public class ExceptionHandleAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            base.OnException(context);
+        }
+    }
+}
